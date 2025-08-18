2a7a6efaa8c00ce77936a5500a3d37c3ef5b672f
Saurav <sauravsaurav0@gmail.com>
2025-08-18 01:30:06 +0000

    Mount the Article route onto the main Express app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 // Import Express
 const express = require('express');
+const articleModel = require('./routes/articleRoute.js')
 
 // Create an Instance of Express
 const app = express();
-
+app.use('/article',articleModel)
 //Export the Express Instance
 module.exports = app;
