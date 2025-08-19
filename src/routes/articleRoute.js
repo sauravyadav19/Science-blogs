73ffbc9a1aae929d4923b9153b43c663bda46fa1
Saurav <sauravsaurav0@gmail.com>
2025-08-19 11:33:03 +0000

    Add paths for Read operation on the Articles

--- src/routes/articleRoute.js
+++ src/routes/articleRoute.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const Article = require('../models/articleModel.js');
-const User = require('../models/userModel.js');
 const articleRoute = express.Router();
+const articleController = require('../controllers/articleControllers.js')
 
-articleRoute.get('/',async(req,res)=>{
-    const allArticles = await Article.find().populate('author');
-    res.send(allArticles);
-})
+articleRoute.get('/',articleController.getArticle);
+articleRoute.get('/:id',articleController.getArticleById);
+
 
 module.exports = articleRoute;
