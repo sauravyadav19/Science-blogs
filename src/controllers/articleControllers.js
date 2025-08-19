919be93384a6e4d40cd8fea85249d68d37db100d
Saurav <sauravsaurav0@gmail.com>
2025-08-19 13:32:51 +0000

    Add Create functionality for Articles

--- src/controllers/articleControllers.js
+++ src/controllers/articleControllers.js
@@ -1,40 +1,54 @@
 const mongoose = require('mongoose')
-const Article = require('../models/articleModel'); 
+const Article = require('../models/articleModel');
 const User = require('../models/userModel.js')
-const Comment = require('../models/commentModel.js')
+const Comment = require('../models/commentModel.js');
 
-exports.getArticle= async(request,response) =>{
-    try{
-        const allArticle = await Article.find().populate('author','username')
-        return response.status(200).json(allArticle);
-    }catch(error){
-       return response.status(500).json({message:`ERROR ${error}`})
-    }
+
+exports.getArticle = async (request, response) => {
+   try{
+      const allArticle = await Article.find().populate('author', 'username');
+      return response.status(200).json(allArticle);
+   }catch(error) {
+      return response.status(500).json({ message: `ERROR ${error}`});
+   }
 }
-exports.getArticleById = async (request,response)=>{
-    try{
-    const id = request.params.id
-    // check upfront if ID is valid
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return response.status(400).json({ message: 'Invalid article ID' });
-    }
-    const article = await Article.findById(id)
-          .populate('author','username email')
-              .populate(
-                {path:'comments',
-                 select:'body',
-                    populate:{
-                        path:'author',
-                        select: 'username'
-                    }
-                });
-    if(!article){
-        return response.status(404).json({message:"There is no such Article"});
-    }
-    else{
-        return response.status(200).json(article);
-    }
-    }catch(error){
-        return response.status(500).json({message:`ERROR ${error}`})
-    }
+exports.getArticleById = async (request, response) => {
+   try{
+      const id = request.params.id
+      // check upfront if ID is valid
+      if(!mongoose.Types.ObjectId.isValid(id)) {
+         return response.status(400).json({message: 'Invalid article ID'});
+      }
+      const article = await Article.findById(id)
+         .populate('author', 'username email')
+         .populate({
+            path: 'comments',
+            select: 'body',
+            populate: {
+               path: 'author',
+               select: 'username'
+            }
+         });
+      if(!article) {
+         return response.status(404).json({message: "There is no such Article"});
+      } else {
+         return response.status(200).json(article);
+      }
+   }catch(error) {
+      return response.status(500).json({message: `ERROR ${error}`});
+   }
+}
+exports.createArticle = async (request, response) => {
+   try{
+      const {title,body,author} = request.body;
+      const newArticle = new Article({
+         title: title,
+         body: body,
+         author: author
+      })
+      await newArticle.save()
+      return response.json({message: "The post has been successfully created"});
+   }catch(error) {
+      response.json(`Error ${error}`)
+   }
 }
