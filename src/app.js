a1aae1109f030f124c61a5896fdfb3df7825c138
Saurav <sauravsaurav0@gmail.com>
2025-08-19 13:34:33 +0000

    Add the Middleware to Parse the JSON on incoming request

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const articleModel = require('./routes/articleRoute.js')
 
 // Create an Instance of Express
 const app = express();
+app.use(express.json())
 app.use('/article',articleModel)
 //Export the Express Instance
 module.exports = app;
