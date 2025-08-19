4b1ab8c3a877684f6d32000558c6c12b824fc75f
Saurav <sauravsaurav0@gmail.com>
2025-08-19 14:35:38 +0000

    Create a controller to edit the an article

--- src/controllers/articleControllers.js
+++ src/controllers/articleControllers.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const Article = require('../models/articleModel');
 const User = require('../models/userModel.js')
 const Comment = require('../models/commentModel.js');
+const { request } = require('express');
 
 
 exports.getArticle = async (request, response) => {
@@ -52,3 +53,34 @@ exports.createArticle = async (request, response) => {
       response.json(`Error ${error}`)
    }
 }
+exports.editArticle = async (request, response) => {
+   try {
+         const id = request.params.id;
+         const { title, body, author } = request.body;
+
+         if (!mongoose.Types.ObjectId.isValid(id)) {
+            return response.status(400).json({ message: 'Invalid Article ID' });
+         }
+
+         const article = await Article.findByIdAndUpdate(
+            id,
+            { $set: { body: body, title: title, author: author } },
+            { new: true, runValidators: true }
+         );
+         // by default mongo return the old doucment setting new to true allows for the
+         // will ask it to send the updated version
+         // and we are re-running the validators so our schema's integrity is maintained
+
+         if (!article) {
+            return response.status(404).json({ message: 'Article Not Found!' });
+         }
+
+         return response.json({
+            message: 'all Changes are successfully saved',
+            article
+         });
+
+   } catch (error) {
+         return response.status(500).json({ message: `Error ${error}` });
+   }
+};
