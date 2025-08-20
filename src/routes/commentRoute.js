ac097bd154f2ac2896528b6227166c8a7fd6322a
Saurav <sauravsaurav0@gmail.com>
2025-08-20 11:47:55 +0000

    Creat comment route handler

---
+++ src/routes/commentRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+// Since in this routes, we need to get the article id from params
+// as the url would look like this /:articleId/comments
+// and we want to access that `articleId` in comments we have to pass this mergeParams options as true
+const commentRoute = express.Router({mergeParams:true});
+const commentController = require('../controllers/commentController.js')
+
+
+commentRoute.get('/', commentController.getAllComments);
+
+module.exports = commentRoute;
