806f66fdcce27f19727414549453e25aa1f73984
Saurav <sauravsaurav0@gmail.com>
2025-08-20 11:56:08 +0000

    Add Comment route within article route

--- src/routes/articleRoute.js
+++ src/routes/articleRoute.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const articleRoute = express.Router();
 const articleController = require('../controllers/articleControllers.js')
+const commentRoute = require('../routes/commentRoute.js');
 
 articleRoute.get('/',articleController.getArticle);
 articleRoute.get('/:id',articleController.getArticleById);
 articleRoute.post('/',articleController.createArticle);
 articleRoute.patch('/:id',articleController.editArticle)
+
+articleRoute.use('/:articleId/comments', commentRoute)
 
 module.exports = articleRoute;
