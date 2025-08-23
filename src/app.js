4a779f73c0eadad7c5815555cc9d702b44d24a5d
Saurav <sauravsaurav0@gmail.com>
2025-08-23 14:21:09 +0000

    Add User Route Handler in the Top level Router

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 // Import Express
 const express = require('express');
 const articleRoute= require('./routes/articleRoute.js')
+const userRoute = require('./routes/userRoute.js')
 
 // Create an Instance of Express
 const app = express();
 app.use(express.json())
 app.use('/article',articleRoute)
+app.use('/users', userRoute);
+
 //Export the Express Instance
 module.exports = app;
