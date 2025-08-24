16ddebb6c7d90953f85de8a1549a90a12780eba6
Saurav <sauravsaurav0@gmail.com>
2025-08-24 10:44:23 +0000

    Set up the veiw engine and middleware to parse incoming form data

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@
 const express = require('express');
 const articleRoute= require('./routes/articleRoute.js')
 const userRoute = require('./routes/userRoute.js')
+const path = require('path')
 
 // Create an Instance of Express
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'/views') )
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use('/article',articleRoute)
 app.use('/users', userRoute);
 
